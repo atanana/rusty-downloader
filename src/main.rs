c7e89ce5c69f9ad984f1f146810da446cb810348
Andrei Tanana <andrei.tanana@deltatre.it>
2019-01-09 16:31:28 +0300

    download page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use reqwest;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let link: String = args[1].parse().unwrap();
+    let folder: String = args[2].parse().unwrap();
+    let page = download_page(&link).unwrap();
+    println!("{}", page);
+}
+
+fn download_page(link: &String) -> Result<String, reqwest::Error> {
+    Ok(reqwest::get(link)?.text()?)
 }
