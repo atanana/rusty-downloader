1d145d38668baece4c4953eea1f87c2b63622890
Andrei <tanana@email.ru>
2019-01-09 22:24:55 +0300

    collect video ids init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::env;
 use reqwest;
 use select::document::Document;
-use select::predicate::{Predicate, Attr, Class, Name};
+use select::predicate::{Attr, Class, Name, Predicate};
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let link: String = args[1].parse().unwrap();
     let folder: String = args[2].parse().unwrap();
-    let pages_count = download_page(&link)
-        .and_then(|page| parse_pages_count(&page)).unwrap();
-    println!("{}", pages_count)
+    let video_ids = download_page(&link)
+        .and_then(|page| parse_pages_count(&page))
+        .map(|pages_count| download_all_pages(&link, pages_count))
+        .unwrap();
+    println!("{:?}", video_ids);
 }
 
 fn download_page(link: &String) -> Option<Document> {
@@ -23,9 +25,23 @@ fn parse_pages_count(document: &Document) -> Option<u32> {
         .descendant(Name("ul"))
         .descendant(Name("li"))
         .descendant(Name("a"));
-    return document.find(link_selector)
+    return document
+        .find(link_selector)
         .map(|link| link.text())
         .map(|link| link.parse::<u32>())
         .filter_map(Result::ok)
         .max();
+}
+
+fn download_all_pages(link: &String, pages_count: u32) -> Vec<u32> {
+    (1..=pages_count)
+        .map(|page| format!("{}&page={}", link, page))
+        .flat_map(|page_link| {
+            download_page(&page_link).map_or(Vec::new(), |doc| parse_video_ids(&doc))
+        })
+        .collect()
+}
+
+fn parse_video_ids(document: &Document) -> Vec<u32> {
+    vec![0]
 }
