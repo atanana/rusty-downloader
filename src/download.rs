2af459deda931a4d46aa214133bf0454d09a52df
Andrei <tanana@email.ru>
2019-01-12 15:29:34 +0300

    refactoring file download

--- src/download.rs
+++ src/download.rs
@@ -1,8 +1,10 @@
 use reqwest;
 use select::document::Document;
 use std::fs;
-use std::io;
+use std::fs::File;
+use std::io::{Error, ErrorKind, copy};
 use std::path::Path;
+use std::error::Error as StdError;
 
 pub fn download_page(link: &String) -> Option<Document> {
     reqwest::get(link)
@@ -14,33 +16,33 @@ fn parse_page(mut response: reqwest::Response) -> Option<Document> {
     Document::from_read(response).ok()
 }
 
-pub fn download_video(link: &String, video_id: &u32, folder_name: &String) -> Result<u64, io::Error> {
-    let mut response = match reqwest::get(link) {
-        Ok(response) => response,
-        Err(e) => return Err(io::Error::new(io::ErrorKind::ConnectionRefused, e))
-    };
-    let mut dest = {
-        let path = Path::new(folder_name).join(format!("{}.mp4", video_id));
-        fs::File::create(path)?
-    };
-    return io::copy(&mut response, &mut dest);
-}
-
 #[derive(Deserialize, Debug)]
 struct DownloadResponse {
     url: String,
     zona: bool,
 }
 
-pub fn get_download_link(client: &reqwest::Client, download_link: &String, video_id: &u32) -> Option<String> {
+fn get_download_link(client: &reqwest::Client, download_link: &String, video_id: &u32) -> Result<String, Box<StdError>> {
     let params = [
         ("id", video_id.to_string()),
-        ("type", "mp4".to_owned())
+        ("type", String::from("mp4"))
     ];
-    return client.post(download_link)
+    let mut response = client.post(download_link)
         .form(&params)
-        .send()
-        .ok()
-        .and_then(|mut response| response.json::<DownloadResponse>().ok())
-        .map(|json| json.url);
+        .send()?;
+    let json: DownloadResponse = response.json()?;
+    return Ok(json.url);
+}
+
+fn create_file(folder_name: &String, video_id: &u32) -> Result<File, Box<StdError>> {
+    let path = Path::new(folder_name).join(format!("{}.mp4", video_id));
+    Ok(File::create(path)?)
+}
+
+pub fn download_video(client: &reqwest::Client, download_link: &String, video_id: &u32, folder_name: &String) -> Result<String, Box<StdError>> {
+    let link = get_download_link(client, download_link, video_id)?;
+    let mut response = reqwest::get(&link)?;
+    let mut file = create_file(folder_name, video_id)?;
+    copy(&mut response, &mut file)?;
+    return Ok(String::from("tst"));
 }
