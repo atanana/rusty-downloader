7903b08ea37389bc2b8b1594abe539d245ddeca8
Andrei <tanana@email.ru>
2019-01-12 15:32:58 +0300

    return downloaded filename

--- src/download.rs
+++ src/download.rs
@@ -34,15 +34,16 @@ fn get_download_link(client: &reqwest::Client, download_link: &String, video_id:
     return Ok(json.url);
 }
 
-fn create_file(folder_name: &String, video_id: &u32) -> Result<File, Box<StdError>> {
-    let path = Path::new(folder_name).join(format!("{}.mp4", video_id));
+fn create_file(folder_name: &String, file_name: &String) -> Result<File, Box<StdError>> {
+    let path = Path::new(folder_name).join(file_name);
     Ok(File::create(path)?)
 }
 
 pub fn download_video(client: &reqwest::Client, download_link: &String, video_id: &u32, folder_name: &String) -> Result<String, Box<StdError>> {
     let link = get_download_link(client, download_link, video_id)?;
     let mut response = reqwest::get(&link)?;
-    let mut file = create_file(folder_name, video_id)?;
+    let file_name = format!("{}.mp4", video_id);
+    let mut file = create_file(folder_name, &file_name)?;
     copy(&mut response, &mut file)?;
-    return Ok(String::from("tst"));
+    return Ok(file_name);
 }
